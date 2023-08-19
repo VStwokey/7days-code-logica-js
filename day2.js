57e05cbc78a2e19f32838dc5128f492e284812c3
Vinicius Santana <Vinicius.Lima33@Outlook.com>
2023-08-19 17:28:19 -0300

    Dia 2

---
+++ day2.js
@@ -0,0 +1,11 @@
+let nome = prompt('Qual o seu nome completo?')
+let idade = parseInt(prompt('Qual a sua idade?'))
+let pergunta = prompt('Qual linguagem de programação você está estudando?')
+
+
+console.log(nome)
+console.log(idade)
+console.log(pergunta)
+
+
+console.log('Olá', nome,'você tem', idade, 'anos e já está aprendendo React!')
